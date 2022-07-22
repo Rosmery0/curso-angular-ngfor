55f097c178ce421b7aeab6601df277a2c779a283
Rosmery0 <rosmery.r@outlook.es>
2022-07-21 21:00:56 -0400

    Binding data from app.component.ts to the table in app.component.html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ngForProject';
+  studentsList:any[]=[
+    {name:'Rosmery Rosario', state:'Regular'},
+    {name:'Jennifer Rosario', state:'Approved'},
+    {name:'Luis Daniel Rosario', state:'Repproved'},
+    {name:'Joselyn Estrella', state:'Normal'},
+  ]
 }
